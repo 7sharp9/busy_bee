3761ff4c74ff40b3c3e31024b471892bc15d7bf2
Dave Thomas <kukulcanenator@googlemail.com>
2020-01-09 21:07:30 +0000

    Add sound register select to mmu

--- src/mmu.rs
+++ src/mmu.rs
@@ -9,8 +9,9 @@ const VIDEO_DISPLAY_REGISTER_END: u32 = 0xFF8260;
 const RESERVED: u32 = 0xFF8400;
 const DMA_DISK_CONTROLLER: u32 = 0xFF8600;
 
-const YM2149_START: u32 = 0xFF8800;
-const YM2149_END: u32 = 0xFF8804;
+const PSG_REGISTER_SELECT: u32 = 0xFF8800;
+const PSG_READ: u32 = PSG_REGISTER_SELECT;
+const PSG_WRITE: u32 = 0xFF8802;
 
 //Multi Function Peripheral
 const MPF68901: u32 = 0xFFFA00;
@@ -25,16 +26,60 @@ const CART_START: u32 = 0xfa0000;
 const CART_END: u32 = 0xFBFFFF;
 
 const MAX_MEMORY: u32 = 0xffffff;
+
 const MAX_ROM: u32 = 0x3ffff;
 const MAX_CART: u32 = 0xFBFFFF;
 
 #[derive(Clone)]
 pub struct Mmu {
+    pub memory_configuration: u8,
     pub rom: Vec<u8>,
     pub cart: Vec<u8>,
     pub memory: Vec<u8>,
-    pub ym2149: [u8; 4],
-    pub video_display: [u8; 96],
+    pub sound: Sound,
+}
+
+#[derive(Clone)]
+pub struct Sound {
+    reg_select: u8,
+    registers: [u8; 16]
+}
+
+impl Sound {
+    pub fn select_register(& mut self, reg: u8) {
+        self.reg_select = reg
+    }
+    pub fn read_data(&self) -> u8 {
+        self.registers[self.reg_select as usize]
+    }
+    pub fn write_data(&mut self, data:u8) {
+        self.registers[self.reg_select as usize] = data
+    }
+
+    pub fn new() -> Sound {
+        Sound {
+            reg_select: 0,
+            registers: [0; 16]
+        }
+    }
+    //0,1 pitch of analogue A 0-7 frequency lower 4 bits of 1 = step size
+    //2,3 pitch of analogue B "
+    //4,5 pitch of analogue C ""
+    //6 lowest 5 bits noise generator, smaller value = higher pitch
+    //7 0 - channel a tone on/off 0 = on
+    //  1 - channel b "
+    //  2 - channel c "
+    //  3 - channel a noise on/off 0 = on
+    //  4 - channel b "
+    //  5 - channel c "
+    //  6 - port a in/output 0 = in 1 = out
+    //  7 - port b "
+    //8 channel a 0-3 register control bit 4 = envelope register is being used and bits 0-3 ignored
+    //9 channel b
+    //10 channel c
+    //11,12 low/high byte of sustain
+    //13 bits 0-3 envelope generator
+    //14,15 PortA / Port B set via bits 7/8 of register 7
 }
 
 impl fmt::Debug for Mmu {
@@ -71,11 +116,13 @@ impl Mmu {
             CART_START..=CART_END => {
                 //TODO
                 0xff
-            }
+            },
+            PSG_READ => self.sound.read_data(), 
             VIDEO_DISPLAY_REGISTER_START..=VIDEO_DISPLAY_REGISTER_END => {
                 unimplemented!("video read");
                 //self.video_display[(address) as usize]
             }
+            0xff8001 => self.memory_configuration,
             a => self.memory[a as usize],
         }
     }
@@ -86,8 +133,9 @@ impl Mmu {
             a if a < 8 => panic!("Memory error:${0:08x}, {0}, {0:024b}", destination),
             ROM_START..ROM_END => panic!("Attempt to write to Rom: ${:08x}", destination),
             CART_START..CART_END => panic!("Attempt to write to Cart: ${:08x}", destination),
+            PSG_REGISTER_SELECT => self.sound.select_register(data),
+            PSG_WRITE => self.sound.write_data(data),
             VIDEO_DISPLAY_REGISTER_START..VIDEO_DISPLAY_REGISTER_END => unimplemented!("write to Video Display Register: {:0x}", address),
-            //YM2149 => ym2149IOMemory[(destination-ym2149Start)] = data
             _ => self.memory[destination as usize] = data,
         }
     }
@@ -137,7 +185,7 @@ impl Mmu {
     }
 
     pub fn read_long(&self, address: u32) -> u32 {
-        match address {
+        match address & 0xffffff {
             0..7 => long_from_slice(&self.rom, address),
             ROM_START..ROM_END => {
                 let address = address & 0x3ffff;
